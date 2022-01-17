f25198b24cfc1be52648dec7229bb7cbd52853d9
Matt Russell <matt.russell@glean.co>
2022-01-17 08:56:56 +0000

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
+    '\\.(css|less|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+  },
+}
